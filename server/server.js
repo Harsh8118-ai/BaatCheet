fcc63d4c2ba2cc88918e691465000938aa12fc2d
Harsh Tyagi <tyagih7654321@gmail.com>
2025-06-19 22:44:56 +0530

    Final Code

--- server/server.js
+++ server/server.js
@@ -148,7 +148,7 @@ app.use((error, req, res, next) => {
 connectDb().then(() => {
   const PORT = process.env.PORT || 3000;
 
-  server.listen(PORT, () => {
+  server.listen(PORT, '127.0.0.1', () => {
     console.log(`🚀 Server is running at port:${PORT}`);
   });
 });
